bdf737faea7694971d8d0b8eaa224366da28545a
Rüzgar Batu Kurt <ruzgarbatukurt@gmail.com>
2025-04-16 21:09:20 +0300

    EncryptionAlgorithm 1.2.py
    - Added a simple user input system that allows the user to use their 
      own key when encrypting a message,
    
    -  Added a while loop to keep the program running until the user 
       wants to turn it off,

--- EncryptionAlgorithmHomework.py
+++ EncryptionAlgorithmHomework.py
@@ -1,7 +1,6 @@
 import random
 import string
-
-#TR
+# TR
 # Rüzgar Batu Kurt tarafından yazılmıştır.
 # Bu kod, bir mesajı şifrelemek ve çözmek için bir anahtar kullanır.
 # Kullanıcıdan alınan mesaj, belirli bir karakter kümesi kullanılarak şifrelenir ve daha sonra çözülür.
@@ -14,57 +13,76 @@ import string
 # Öğrenmeye ve geliştirmeye devam ediyorum.
 # Eğlenceli bir deneyim olması dileğiyle.
 # :)-------------------------------------------------------------------------------------------------------------------
-#ENG
-# Written by Rüzgar Batu Kurt.
-# This code uses a key to encrypt and decrypt a message.
-# The message received from the user is encrypted using a specific character set and then copied.
-# The key is created by mixing the character set.
-# The message received from the user is encrypted with the key and then copied.
-# The key, passwords and fragments received from the user are used.
-# This code is written in the Python programming language and is an example to understand basic encryption software.
-# It can be run on Phyton 3.10.0 and above.
-# I am new to Python and software in general, please do not question too much beyond the limit of the code. I am aware of the complicated elements.
-# I am learning and continuing to develop my skills.
-# I wish you a fun experience.
-# :)------------------------------------------------------------------------------------------------------
+# ENG
+# Code Written By Rüzgar Batu Kurt
+# This code uses a randomly generated key to encrypt messages provided by the user.
+# The input provided by the user is encrypted with the use of a list of characters.
+# The key is formed by shuffling the character list in a random order.
+# The Key is used to encrypt and decrypt messages in an organised combination
+# This code was written in phyton (if that wasnt obvious)
+# Will work on any version 3.10.0 and above.
+# Im new to phyton and programming in general, please DO NOT question far too beyond the quality of the code, I am aware of the complicated elements.
+# Im still learning and developing my skills at this time.
+# Most of the language used in this code is turkish, (variables etc.) If you don't know turkish, I recommend using an ai to translate such things.
+# I wish you a fun experience,
 
 
 chars = " " + string.punctuation + string.ascii_letters + string.digits
 chars = list(chars)
 
-# Anahtar oluşturma
-anahtar = chars.copy()
-random.shuffle(anahtar)
 
-# Anahtarı ekrana yazdırma
-print("Anahtar:")
-print("".join(anahtar))
+print("Anahtar Seçimi:")
+print("1. Rastgele bir anahtar oluştur")
+print("2. Kendi anahtarını gir")
+print("Programdan çıkmak için 3'e basın")
 
-# Şifreleme Kısmı
-mesaj = input("Lütfen şifrelenecek mesajı giriniz: ")
-sifreli_mesaj = ""
+while True: 
+    choice = input("Seçiminizi yapın (1/2/3): ")
 
-for letter in mesaj:
-    index = chars.index(letter)
-    sifreli_mesaj += anahtar[index]
+    if choice == "1":
+        
+        anahtar = chars.copy()
+        random.shuffle(anahtar)
+        print("Oluşturulan Rastgele Anahtar:")
+        print("".join(anahtar))
 
-print("Şifreli mesaj: ", sifreli_mesaj)
-print("Orijinal mesaj:", mesaj)
+    elif choice == "2":
+        
+        anahtar_input = input("Lütfen kendi anahtarınızı giriniz: ")
+        if len(anahtar_input) != len(chars):
+            print("Hatalı anahtar! Anahtar, karakter kümesiyle aynı uzunlukta olmalıdır.")
+            exit()
+        anahtar = list(anahtar_input)
 
-# Çözümleme Kısmı
-print("\n--- Çözümleme Kısmı ---")
-anahtar_input = input("Lütfen çözümleme için anahtarı giriniz: ")
-if len(anahtar_input) != len(chars):
-    print("Hatalı anahtar! Lütfen doğru anahtarı giriniz.")
-    exit()
+    elif choice == "3":
+        print("Program kapatılıyor...")
+        exit()
 
-anahtar = list(anahtar_input)  # Kullanıcıdan alınan anahtarı kullan
+    
+    mesaj = input("Lütfen şifrelenecek mesajı giriniz: ")
+    sifreli_mesaj = ""
 
-sifreli_mesaj = input("Lütfen çözümlenecek mesajı giriniz: ")
-mesaj = ""
+    for letter in mesaj:
+        index = chars.index(letter)
+        sifreli_mesaj += anahtar[index]
 
-for letter in sifreli_mesaj:
-    index = anahtar.index(letter)
-    mesaj += chars[index]
+    print("Şifreli mesaj: ", sifreli_mesaj)
+    print("Orijinal mesaj:", mesaj)
 
-print("Çözümlenmiş mesaj: ", mesaj)
+    
+    print("\n--- Çözümleme Kısmı ---")
+    anahtar_input = input("Lütfen çözümleme için anahtarı giriniz: ")
+    if len(anahtar_input) != len(chars):
+        print("Hatalı anahtar! Lütfen doğru anahtarı giriniz.")
+        exit()
+
+    anahtar = list(anahtar_input)
+
+    sifreli_mesaj = input("Lütfen çözümlenecek mesajı giriniz: ")
+    mesaj = ""
+
+    for letter in sifreli_mesaj:
+        index = anahtar.index(letter)
+        mesaj += chars[index]
+
+    print("Çözümlenmiş mesaj: ", mesaj)
