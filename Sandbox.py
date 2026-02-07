afd4c5357a2a64813ca2b797e2f66d11ed8feeca
Rüzgar Batu Kurt <ruzgarbatukurt@gmail.com>
2026-02-07 18:38:30 +0300

    Add files via upload

---
+++ Sandbox.py
@@ -0,0 +1,30 @@
+import requests
+
+#API key: ae4ec26fc343f04a223249613629346f
+
+API_KEY = "ae4ec26fc343f04a223249613629346f"
+city = str(input("Enter city name:")).lower()
+
+default_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+metric_choice = str(input("Choose your metric system - Metric/Imperial/Default(Kelvin):")).lower()
+
+if metric_choice == "metric":
+    url = default_url + "&units=metric"
+elif metric_choice == "imperial":
+    url = default_url + "&units=imperial"
+else:
+    url = default_url
+
+response = requests.get(url)
+info = response.json()
+
+temp = info['main']['temp']
+humidity = info['main']['humidity']
+wind_speed = info['wind']['speed']
+weatherDesc= info['weather'][0]['description']
+
+print(f"Temperature: {temp}")
+print(f"Humidity: {humidity}")
+print(f"Wind Speed: {wind_speed}")
+print(f"Weather description: {weatherDesc}")
+
